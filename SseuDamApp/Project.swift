48d5039bcf0e92c8cf4b44256fc4a03b41e9910f
홍석현 <ghdtjrgus333@naver.com>
2025-11-14 07:37:03 +0900

    feat: SseuDamApp을 SwiftUI로 전환
    - UIKit AppDelegate를 SwiftUI App 구조로 변경
    - ContentView를 메인 SwiftUI 뷰로 추가
    - SceneDelegate 제거
    - Project.swift에서 SwiftUI용 infoPlist 설정으로 업데이트

--- SseuDamApp/Project.swift
+++ SseuDamApp/Project.swift
@@ -1,29 +1,8 @@
 import ProjectDescription
+import ModularAppPlugin
 
-let project = Project(
+let project = Project.makeApp(
     name: "SseuDamApp",
-    targets: [
-        .target(
-            name: "SseuDamApp",
-            destinations: .iOS,
-            product: .app,
-            bundleId: "com.testdev.sseudamapp",
-            infoPlist: .extendingDefault(with: [
-                "UILaunchStoryboardName": "LaunchScreen",
-                "UIApplicationSceneManifest": [
-                    "UIApplicationSupportsMultipleScenes": false,
-                    "UISceneConfigurations": [
-                        "UIWindowSceneSessionRoleApplication": [
-                            [
-                                "UISceneConfigurationName": "Default Configuration",
-                                "UISceneDelegateClassName": "$(PRODUCT_MODULE_NAME).SceneDelegate"
-                            ]
-                        ]
-                    ]
-                ]
-            ]),
-            sources: ["Sources/**"],
-            resources: ["Resources/**"]
-        )
-    ]
+    dependencies: [],
+    infoPlist: .defaultSwiftUIApp
 )
