d9dcef366e67102496654c60721e3f8e740c6185
홍석현 <ghdtjrgus333@naver.com>
2025-11-14 07:37:11 +0900

    feat: Tuist app 템플릿을 SwiftUI 기반으로 수정
    - appDelegate.stencil을 app.stencil로 변경하여 SwiftUI App 구조 생성
    - contentView.stencil 추가
    - sceneDelegate.stencil 제거
    - project.stencil에서 SceneDelegate 설정 제거

--- Tuist/Templates/app/Template.swift
+++ Tuist/Templates/app/Template.swift
@@ -3,14 +3,14 @@ import ProjectDescription
 fileprivate let nameAttribute: Template.Attribute = .required("name")
 
 fileprivate let template = Template(
-    description: "A template for a new UIKit application.",
+    description: "A template for a new SwiftUI application.",
     attributes: [
         nameAttribute,
     ],
     items: [
         .file(path: "Project.swift", templatePath: "project.stencil"),
-        .file(path: "Sources/AppDelegate.swift", templatePath: "appDelegate.stencil"),
-        .file(path: "Sources/SceneDelegate.swift", templatePath: "sceneDelegate.stencil"),
+        .file(path: "Sources/\(nameAttribute)App.swift", templatePath: "app.stencil"),
+        .file(path: "Sources/ContentView.swift", templatePath: "contentView.stencil"),
         .file(path: "Resources/Base.lproj/LaunchScreen.storyboard", templatePath: "launchScreen.stencil"),
         .file(path: "Resources/Assets.xcassets/Contents.json", templatePath: "assets_root.stencil"),
         .file(path: "Resources/Assets.xcassets/AppIcon.appiconset/Contents.json", templatePath: "assets_appicon.stencil"),
