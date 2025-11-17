557dd42cdbc07a3eec62fc6d732aaf871490afba
minneee <alsgml022100@naver.com>
2025-11-17 20:30:18 +0900

    [feat] 데이터 매핑

---
+++ Data/Sources/Network/Formatter/DateFormatters.swift
@@ -0,0 +1,25 @@
+//
+//  DateFormatters.swift
+//  Data
+//
+//  Created by 김민희 on 11/17/25.
+//
+
+import Foundation
+
+enum DateFormatters {
+  static let apiDate: DateFormatter = {
+    let f = DateFormatter()
+    f.calendar = .init(identifier: .gregorian)
+    f.locale = Locale(identifier: "ko_KR")
+    f.timeZone = TimeZone(secondsFromGMT: 0)
+    f.dateFormat = "yyyy-MM-dd"
+    return f
+  }()
+
+  static let apiDateTime: ISO8601DateFormatter = {
+    let f = ISO8601DateFormatter()
+    f.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+    return f
+  }()
+}
