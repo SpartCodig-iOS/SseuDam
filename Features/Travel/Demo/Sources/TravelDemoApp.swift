0e68e8ffed4004082de72e4bddd9fc453f3f9147
minneee <alsgml022100@naver.com>
2025-11-17 15:28:54 +0900

    [feat] travel feature 생성

---
+++ Features/Travel/Demo/Sources/TravelDemoApp.swift
@@ -0,0 +1,21 @@
+//
+//  TravelDemoApp.swift
+//  SseuDam
+//
+//  Created by SseuDam on2025.
+//  Copyright ©2025 com.testdev. All rights reserved.
+//
+
+import SwiftUI
+import TravelFeature
+
+@main
+struct TravelDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                TravelView()
+            }
+        }
+    }
+}
