09cb4871c012bce5155dae092f7cddd28bd1d563
Roy <shuwj81@daum.net>
2025-11-18 10:31:41 +0900

    [feat]: font 추가 및  info plist 변경

--- Tuist/Plugins/SseuDamPlugin/ProjectDescriptionHelpers/InfoPlist+Helpers.swift
+++ Tuist/Plugins/SseuDamPlugin/ProjectDescriptionHelpers/InfoPlist+Helpers.swift
@@ -8,13 +8,11 @@ public extension InfoPlist {
           "UILaunchScreen": .dictionary([:])
         ]),
 
-        // 환경 변수 → Info.plist 로 흘려보내기
         "SUPERBASE_URL": .string("$(SUPERBASE_URL)"),
         "SUPERBASE_KEY": .string("$(SUPERBASE_KEY)"),
         "GOOGLE_IOS_CLIENT_ID": .string("$(GOOGLE_IOS_CLIENT_ID)"),
         "GOOGLE_SERVER_CLIENT_ID": .string("$(GOOGLE_SERVER_CLIENT_ID)"),
         "GOOGLE_REVERSED_IOS_CLIENT_ID": .string("$(GOOGLE_REVERSED_IOS_CLIENT_ID)"),
-        // CFBundleURLTypes (네가 첫 번째에 쓴 구조 그대로)
         "CFBundleURLTypes": .array([
           .dictionary([
             "CFBundleURLName": .string("sseudam"),
@@ -28,7 +26,10 @@ public extension InfoPlist {
               .string("$(GOOGLE_REVERSED_IOS_CLIENT_ID)"),
             ])
           ])
-        ])
+        ]),
+        "UIAppFonts": .array([
+           .string("PretendardVariable.ttf")
+         ])
       ])
     }
 
