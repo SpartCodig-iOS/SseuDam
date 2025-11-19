17155759697b68ead38b2fdc7016d458d526c360
minneee <alsgml022100@naver.com>
2025-11-19 15:46:08 +0900

    [feat] 공용 Response모델 생성

---
+++ NetworkService/Sources/Response/MetaDTO.swift
@@ -0,0 +1,13 @@
+//
+//  MetaDTO.swift
+//  NetworkService
+//
+//  Created by 김민희 on 11/19/25.
+//
+
+import Foundation
+
+public struct MetaDTO: Decodable {
+    public let responseTime: String?
+    public let cached: Bool?
+}
