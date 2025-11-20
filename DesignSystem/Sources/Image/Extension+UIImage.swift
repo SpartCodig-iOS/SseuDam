8418db6af6bbb31752e25965a6a83f302ca3515d
Roy <shuwj81@daum.net>
2025-11-20 20:11:53 +0900

    [chore]: designsystem 오류 수정

--- DesignSystem/Sources/Image/Extension+UIImage.swift
+++ DesignSystem/Sources/Image/Extension+UIImage.swift
@@ -6,62 +6,23 @@
 //
 
 import SwiftUI
-import UIKit
 
-public extension UIImage {
-  convenience init?(_ asset: ImageAsset) {
-    self.init(named: asset.rawValue, in: Bundle.module, with: nil)
-  }
-
-  convenience init?(assetName: String) {
-    self.init(named: assetName, in: Bundle.module, with: nil)
-  }
+public extension Bundle {
+  /// 외부 모듈에서도 사용할 수 있도록 공개 번들 헬퍼
+  static var designSystem: Bundle { .module }
 }
-
-
-// DSResources.swift
-import Foundation
-
-public enum DSResources {
-  public static let bundle: Bundle = .module
-}
-
-import SwiftUI
-#if canImport(UIKit)
-import UIKit
-#endif
-
-#if canImport(UIKit)
-public extension UIImage {
-  /// 문자열 기반 에셋 이름
-  convenience init?(assetName: String, in bundle: Bundle = DSResources.bundle) {
-    self.init(named: assetName, in: bundle, with: nil)
-  }
-
-  convenience init?<Asset: RawRepresentable>(
-    asset: Asset,
-    in bundle: Bundle = DSResources.bundle
-  ) where Asset.RawValue == String {
-    self.init(named: asset.rawValue, in: bundle, with: nil)
-  }
-}
-#endif
-
-
-import SwiftUI
 
 public extension Image {
-  /// 문자열 기반 에셋 이름
-  init(assetName: String, bundle: Bundle = DSResources.bundle) {
+  /// 문자열 기반 에셋 이름을 SwiftUI Image로 로드
+  init(assetName: String, bundle: Bundle = .designSystem) {
     self.init(assetName, bundle: bundle)
   }
 
+  /// RawValue가 String인 enum 등을 사용해 에셋을 로드
   init<Asset: RawRepresentable>(
     asset: Asset,
-    bundle: Bundle = DSResources.bundle
+    bundle: Bundle = .designSystem
   ) where Asset.RawValue == String {
     self.init(asset.rawValue, bundle: bundle)
   }
 }
-
-
