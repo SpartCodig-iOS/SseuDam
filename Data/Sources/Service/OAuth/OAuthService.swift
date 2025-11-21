e723cc9028bd13ec706a21767666d1e87870d860
Roy <shuwj81@daum.net>
2025-11-21 17:56:17 +0900

    [feat]: oauth Service 구현

--- Data/Sources/Service/OAuth/OAuthService.swift
+++ Data/Sources/Service/OAuth/OAuthService.swift
@@ -5,3 +5,43 @@
 //  Created by Wonji Suh  on 11/17/25.
 //
 
+import Moya
+import NetworkService
+
+public enum OAuthService {
+  case checkSignUpUser(body: OAuthCheckUserRequestDTO)
+}
+
+
+extension OAuthService: BaseTargetType {
+  public typealias Domain = SseuDamDomain
+
+  public var domain: SseuDamDomain {
+    return .oauth
+  }
+
+  public var urlPath: String {
+    switch self {
+      case .checkSignUpUser:
+        return OAuthAPI.checkSignUpUser.description
+    }
+  }
+  
+  public var error: [Int : NetworkService.NetworkError]? {
+    return nil
+  }
+  
+  public var parameters: [String : Any]? {
+    switch self {
+      case .checkSignUpUser(let body):
+        return body.toDictionary
+    }
+  }
+  
+  public var method: Moya.Method {
+    switch self {
+      case .checkSignUpUser:
+        return .post
+    }
+  }
+}
