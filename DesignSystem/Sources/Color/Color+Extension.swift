0b18aa8d2d204d55356d79cade1a9a0511f5192b
Roy <shuwj81@daum.net>
2025-11-24 20:22:53 +0900

    [feat]: 컬러 추가

--- DesignSystem/Sources/Color/Color+Extension.swift
+++ DesignSystem/Sources/Color/Color+Extension.swift
@@ -7,7 +7,8 @@ public extension ShapeStyle where Self == Color {
     static var primary50: Color {asset(#function) }
     static var primary500: Color {asset(#function) }
     static var gray2: Color {asset(#function)}
-    
+    static var gray8: Color {asset(#function)}
+
     private static func asset(_ name: String) -> Color {
         Color(name, bundle: .module)
     }
