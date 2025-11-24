77cee3f9d7ac7cafce5a474f2ca342a884339069
Roy <shuwj81@daum.net>
2025-11-24 16:40:23 +0900

    [feat]: 로그인 관려 service 추가
    # Conflicts:
    #	Data/Sources/TargetType/OAuth/ OAuthAPITarget.swift

--- Data/Sources/TargetType/OAuth/ OAuthAPITarget.swift
+++ Data/Sources/TargetType/OAuth/ OAuthAPITarget.swift
@@ -8,21 +8,25 @@
 import Moya
 import NetworkService
 
-public enum OAuthAPITarget {
   case checkSignUpUser(body: OAuthCheckUserRequestDTO)
+public enum OAuthAPITarget {
+  case lognOAuth(body: OAuthLoginUserRequestDTO)
 }
 
 extension OAuthAPITarget: BaseTargetType {
   public typealias Domain = SseuDamDomain
-
+  
   public var domain: SseuDamDomain {
     return .oauth
   }
-
+  
   public var urlPath: String {
     switch self {
       case .checkSignUpUser:
         return OAuthAPI.checkSignUpUser.description
+        
+      case .lognOAuth:
+        return OAuthAPI.login.description
     }
   }
   
@@ -34,12 +38,15 @@ extension OAuthAPITarget: BaseTargetType {
     switch self {
       case .checkSignUpUser(let body):
         return body.toDictionary
+        
+      case .lognOAuth(let body):
+        return body.toDictionary
     }
   }
   
   public var method: Moya.Method {
     switch self {
-      case .checkSignUpUser:
+      case .checkSignUpUser, .lognOAuth:
         return .post
     }
   }
