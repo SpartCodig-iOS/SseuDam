ed49d1f384d36a55ff315a02b3ae3e230fd7243e
홍석현 <ghdtjrgus333@naver.com>
2025-11-27 11:12:42 +0900

    bugfix: 디자인 시스템 색상 에셋 이름 대소문자 불일치 수정

--- DesignSystem/Sources/Color/Color+Extension.swift
+++ DesignSystem/Sources/Color/Color+Extension.swift
@@ -4,9 +4,9 @@ import SwiftUI
 public extension ShapeStyle where Self == Color { 
     static var primary800: Color { asset(#function) }
     static var primary900: Color { asset(#function) }
-    static var primary50: Color {asset(#function) }
-    static var primary500: Color {asset(#function) }
-    static var gray2: Color {asset(#function)}
+    static var primary50: Color { asset(#function) }
+    static var primary500: Color { asset(#function) }
+    static var gray2: Color { asset(#function) }
     
     private static func asset(_ name: String) -> Color {
         Color(name, bundle: .module)
