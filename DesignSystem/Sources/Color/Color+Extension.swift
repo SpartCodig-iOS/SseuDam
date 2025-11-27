02b0084164bec50c67e11a4c5bf3241bbe55084c
Roy <shuwj81@daum.net>
2025-11-27 14:43:16 +0900

    [feat]: 컬러 추가 및 주석 삭제

--- DesignSystem/Sources/Color/Color+Extension.swift
+++ DesignSystem/Sources/Color/Color+Extension.swift
@@ -8,10 +8,7 @@ public extension ShapeStyle where Self == Color {
     static var primary500: Color {asset(#function) }
     static var gray2: Color {asset(#function)}
     static var gray8: Color {asset(#function)}
-    static var primary50: Color { asset(#function) }
-    static var primary500: Color { asset(#function) }
-    static var gray2: Color { asset(#function) }
-    
+
     private static func asset(_ name: String) -> Color {
         Color(name, bundle: .module)
     }
