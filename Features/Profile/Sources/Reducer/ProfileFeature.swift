34aefd2892138d09acf66a399487626b3819d3fe
Roy <shuwj81@daum.net>
2025-11-29 00:16:58 +0900

    [chore]: 프로필 모듈 구현 #18
    # Conflicts:
    #	SseuDamApp/Project.swift
    
    # Conflicts:
    #	Tuist/Plugins/SseuDamPlugin/ProjectDescriptionHelpers/TargetDependency+Modules.swift

---
+++ Features/Profile/Sources/Reducer/ProfileFeature.swift
@@ -0,0 +1,115 @@
+//
+//  ProfileFeature.swift
+//  ProfileFeature
+//
+//  Created by Wonji Suh  on 11/28/25.
+//
+
+import Foundation
+import ComposableArchitecture
+
+
+@Reducer
+public struct ProfileFeature {
+  public init() {}
+
+  @ObservableState
+  public struct State: Equatable {
+
+    public init() {}
+  }
+
+  public enum Action: ViewAction, BindableAction {
+    case binding(BindingAction<State>)
+    case view(View)
+    case async(AsyncAction)
+    case inner(InnerAction)
+    case navigation(NavigationAction)
+
+  }
+
+  //MARK: - ViewAction
+  @CasePathable
+  public enum View {
+
+  }
+
+
+
+  //MARK: - AsyncAction 비동기 처리 액션
+  public enum AsyncAction: Equatable {
+
+  }
+
+  //MARK: - 앱내에서 사용하는 액션
+  public enum InnerAction: Equatable {
+  }
+
+  //MARK: - NavigationAction
+  public enum NavigationAction: Equatable {
+
+
+  }
+
+
+  public var body: some Reducer<State, Action> {
+    BindingReducer()
+    Reduce { state, action in
+      switch action {
+        case .binding(_):
+          return .none
+
+        case .view(let viewAction):
+          return handleViewAction(state: &state, action: viewAction)
+
+        case .async(let asyncAction):
+          return handleAsyncAction(state: &state, action: asyncAction)
+
+        case .inner(let innerAction):
+          return handleInnerAction(state: &state, action: innerAction)
+
+        case .navigation(let navigationAction):
+          return handleNavigationAction(state: &state, action: navigationAction)
+      }
+    }
+  }
+}
+
+extension ProfileFeature {
+  private func handleViewAction(
+    state: inout State,
+    action: View
+  ) -> Effect<Action> {
+    switch action {
+
+    }
+  }
+
+  private func handleAsyncAction(
+    state: inout State,
+    action: AsyncAction
+  ) -> Effect<Action> {
+    switch action {
+
+    }
+  }
+
+  private func handleNavigationAction(
+    state: inout State,
+    action: NavigationAction
+  ) -> Effect<Action> {
+    switch action {
+
+    }
+  }
+
+  private func handleInnerAction(
+    state: inout State,
+    action: InnerAction
+  ) -> Effect<Action> {
+    switch action {
+
+    }
+  }
+}
+
