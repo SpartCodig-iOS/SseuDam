0af8a571ff373b81ac409fd93936512a8595a0c4
minneee <alsgml022100@naver.com>
2025-11-30 00:17:16 +0900

    [feat] 데모 앱에 서버 통신 repository 추가

--- Features/Travel/Demo/Sources/TravelDemoApp.swift
+++ Features/Travel/Demo/Sources/TravelDemoApp.swift
@@ -16,12 +16,13 @@ import TravelFeature
 struct TravelDemoApp: App {
 
     private static let mockRepo = MockTravelRepository()
+    private static let repo = TravelRepository(remote: TravelRemoteDataSource())
 
     private let store = Store(initialState: TravelListFeature.State()) {
         TravelListFeature()
     } withDependencies: {
-        $0.fetchTravelsUseCase = FetchTravelsUseCase(repository: mockRepo)
-        $0.createTravelUseCase = CreateTravelUseCase(repository: mockRepo)
+        $0.fetchTravelsUseCase = FetchTravelsUseCase(repository: repo)
+        $0.createTravelUseCase = CreateTravelUseCase(repository: repo)
     }
 
     var body: some Scene {
