d94eb8173884c34d303ea8e38b16901679bf204b
홍석현 <ghdtjrgus333@naver.com>
2025-12-08 16:06:45 +0900

    feat: SettlementCoordinator SaveExpense 완료 처리 개선
    - SaveExpense 완료 시 Toast 메시지 표시
    - 생성/수정 타입에 따른 동적 메시지 처리
    - 뒤로가기 액션 핸들링 추가

--- Features/Settlement/Sources/Coordinator/SettlementCoordinator.swift
+++ Features/Settlement/Sources/Coordinator/SettlementCoordinator.swift
@@ -9,16 +9,17 @@ import Foundation
 import ComposableArchitecture
 import TCACoordinators
 import ExpenseFeature
+import DesignSystem
 
 @Reducer
 public struct SettlementCoordinator {
     public init() {}
-
+    
     @ObservableState
     public struct State: Equatable {
         var routes: [Route<SettlementScreen.State>]
         let travelId: String
-
+        
         public init(travelId: String) {
             self.travelId = travelId
             self.routes = [.root(.settlement(.init(travelId)))]
@@ -41,7 +42,7 @@ public struct SettlementCoordinator {
     public var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
-            // 지출 추가 버튼
+                // 지출 추가 버튼
             case .router(.routeAction(_, .settlement(.delegate(.addExpenseButtonTapped)))):
                 guard let settlementState = state.routes.first?.screen.settlement,
                       let travel = settlementState.travel else {
@@ -63,12 +64,16 @@ public struct SettlementCoordinator {
                 
             case .router(.routeAction(_, .settlement(.delegate(.onTapSettingsButton(let travelId))))):
                 return .send(.delegate(.onTapTravelSettingsButton(travelId: travelId)))
-
-            // 설정 버튼 (여행 상세/수정) - 추후 구현
-             case .router(.routeAction(_, .saveExpense(.delegate(.finishSaveExpense)))):
-                 state.routes.pop()
-                 return .none
-
+            case .router(.routeAction(_, .saveExpense(.delegate(.finishSaveExpense(let type))))):
+                state.routes.pop()
+                return .run { _ in
+                    await MainActor.run {
+                        ToastManager.shared.showSuccess("\(type.displayName)이 완료되었어요.")
+                    }
+                }
+            case .router(.routeAction(_, .saveExpense(.delegate(.onTapBackButton)))):
+                state.routes.pop()
+                return .none
             default:
                 break
             }
