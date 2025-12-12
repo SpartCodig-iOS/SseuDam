dfee5d579ccb5c8da833b0dd9768180263919053
Roy <shuwj81@daum.net>
2025-12-12 12:42:41 +0900

    [feat]: deeplink 리팩토링 firebase 의존성 문제 해결

---
+++ Domain/Sources/Router/DeeplinkRouter.swift
@@ -0,0 +1,97 @@
+//
+//  DeeplinkRouter.swift
+//  Domain
+//
+//  Created by Wonji Suh on 12/12/25.
+//
+
+import Foundation
+import Dependencies
+
+public struct DeeplinkRouter: Sendable {
+
+    public init() {}
+
+    // MARK: - Public Interface
+
+    public func parse(_ urlString: String) -> DeeplinkResult {
+        guard let url = URL(string: urlString),
+              url.scheme == "sseudam" else {
+            return .invalid(url: urlString, reason: "Invalid scheme")
+        }
+
+        let pathComponents = url.pathComponents.filter { $0 != "/" }
+
+        switch url.host ?? pathComponents.first {
+        case "travel":
+            return parseTravelDeeplink(url: url, pathComponents: pathComponents)
+        case "invite":
+            return parseInviteDeeplink(url: url)
+        default:
+            return .success(.unknown(url: urlString))
+        }
+    }
+
+    // MARK: - Private Parsing
+
+    private func parseTravelDeeplink(url: URL, pathComponents: [String]) -> DeeplinkResult {
+        guard let (travelId, remainingComponents) = extractTravelId(url: url, pathComponents: pathComponents),
+              !travelId.isEmpty else {
+            return .invalid(url: url.absoluteString, reason: "Missing travel ID")
+        }
+
+        let travelDeeplink: TravelDeeplink = {
+            switch remainingComponents.first {
+            case "settings":
+                return .settings(travelId: travelId)
+            case "expense" where remainingComponents.count >= 2:
+                return .expense(travelId: travelId, expenseId: remainingComponents[1])
+            case "settlement":
+                return .settlement(travelId: travelId)
+            default:
+                return .detail(travelId: travelId)
+            }
+        }()
+
+        return .success(.travel(travelDeeplink))
+    }
+
+    private func parseInviteDeeplink(url: URL) -> DeeplinkResult {
+        guard let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
+              let inviteCode = components.queryItems?.first(where: { $0.name == "code" })?.value,
+              !inviteCode.isEmpty else {
+            return .invalid(url: url.absoluteString, reason: "Missing invite code")
+        }
+
+        return .requiresLogin(destination: .invite(code: inviteCode))
+    }
+
+    private func extractTravelId(url: URL, pathComponents: [String]) -> (String, [String])? {
+        if pathComponents.first == "travel" && pathComponents.count >= 2 {
+            // ["travel", "123", "expense", "456"]
+            let travelId = pathComponents[1]
+            let remaining = Array(pathComponents.dropFirst(2))
+            return (travelId, remaining)
+        } else if url.host == "travel" && pathComponents.count >= 1 {
+            // host="travel", path=["123", "expense", "456"]
+            let travelId = pathComponents[0]
+            let remaining = Array(pathComponents.dropFirst(1))
+            return (travelId, remaining)
+        }
+        return nil
+    }
+}
+
+// MARK: - Dependencies
+
+extension DeeplinkRouter: DependencyKey {
+    public static let liveValue = DeeplinkRouter()
+    public static let testValue = DeeplinkRouter()
+}
+
+extension DependencyValues {
+    public var deeplinkRouter: DeeplinkRouter {
+        get { self[DeeplinkRouter.self] }
+        set { self[DeeplinkRouter.self] = newValue }
+    }
+}
--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -10,7 +10,9 @@ let packageSettings = PackageSettings(
         "TCACoordinators": .framework,
         "Moya": .framework,
         "LogMacro": .framework,
-
+        "GoogleSignIn": .framework,
+        "AppAuth": .framework,        
+        "GTMAppAuth": .framework
     ]
 )
 #endif
@@ -25,6 +27,6 @@ let package = Package(
         .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "9.0.0"),
         .package(url: "https://github.com/Roy-wonji/LogMacro.git", from: "1.1.1"),
         .package(url: "https://github.com/Moya/Moya.git", exact: "15.0.3"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.0.0")
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "12.7.0")
     ]
 )
