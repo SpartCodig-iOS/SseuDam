cd233b2553304e7710c962b465359c960ceabd32
홍석현 <ghdtjrgus333@naver.com>
2025-12-12 09:36:54 +0900

    chore: 지출 char view 컴포넌트 파일 생성

---
+++ Features/ExpenseList/Sources/Components/ExpenseChartView.swift
@@ -0,0 +1,19 @@
+//
+//  ExpenseChartView.swift
+//  ExpenseListFeature
+//
+//  Created by 홍석현 on 12/11/25.
+//
+
+import SwiftUI
+import Charts
+
+struct ExpenseChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseChartView()
+}
