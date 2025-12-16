a3bb5a887764ce4a2bee012184ad738e26d9f343
홍석현 <ghdtjrgus333@naver.com>
2025-12-16 19:48:04 +0900

    refactor: profile

--- Domain/Sources/Repository/Profile/ProfileRepositoryProtocol.swift
+++ Domain/Sources/Repository/Profile/ProfileRepositoryProtocol.swift
@@ -6,8 +6,25 @@
 //
 
 import Foundation
+import Dependencies
 
 public protocol ProfileRepositoryProtocol {
-  func getProfile() async throws -> Profile
-  func editProfile(name: String?, avatarData: Data?, fileName: String?) async throws -> Profile
+    func getProfile() async throws -> Profile
+    func editProfile(name: String?, avatarData: Data?, fileName: String?) async throws -> Profile
+}
+
+// MARK: - Dependencies
+private struct ProfileRepositoryDependencyKey: DependencyKey {
+    public static var liveValue: ProfileRepositoryProtocol {
+        fatalError("ProfileRepositoryDependency liveValue not implemented")
+    }
+    public static var previewValue: ProfileRepositoryProtocol = MockProfileRepository()
+    public static var testValue: ProfileRepositoryProtocol = MockProfileRepository()
+}
+
+public extension DependencyValues {
+    var profileRepository: ProfileRepositoryProtocol {
+        get { self[ProfileRepositoryDependencyKey.self] }
+        set { self[ProfileRepositoryDependencyKey.self] = newValue }
+    }
 }
