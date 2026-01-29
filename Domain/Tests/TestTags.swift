cf732005486e49c902d8285b59b6cb35c53afc95
홍석현 <ghdtjrgus333@naver.com>
2026-01-29 15:55:48 +0900

    Chore: 테스트 설정 파일 정리
    - TestTags: travel, expense, settlement, auth 태그 추가
    - RecordExpenseUseCaseTests 삭제 (UseCase 삭제됨)

--- Domain/Tests/TestTags.swift
+++ Domain/Tests/TestTags.swift
@@ -12,5 +12,9 @@ extension Tag {
     @Tag static var unit: Self
     @Tag static var useCase: Self
     @Tag static var integration: Self
-    
+    @Tag static var travel: Self
+    @Tag static var expense: Self
+    @Tag static var settlement: Self
+    @Tag static var auth: Self
+    @Tag static var model: Self
 }
